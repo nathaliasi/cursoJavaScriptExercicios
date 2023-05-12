d714bb109d741929604650216e32afb71369d4ac
Nathália Sardou <nathaliatrab@outlook.com>
2023-05-12 17:32:34 -0300

    exercicios array e e iteração

--- script.js
+++ script.js
@@ -1,46 +1,69 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
-// Remova o primeiro valor de comidas e coloque em uma variável
-const removePrimeiro = comidas.shift();
-console.log(removePrimeiro);
-// Remova o último valor de comidas e coloque em uma variável
-const removeUltimo = comidas.pop();
-console.log(removeUltimo);
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const cursos = document.querySelectorAll('.curso');
+const arrayCursos = Array.from(cursos);
 
-// Adicione 'Arroz' ao final da array
-const addFinal = comidas.push('Arroz');
-console.log(comidas);
-// Adicione 'Peixe' e 'Batata' ao início da array
-const addInicio = comidas.unshift('Peixe', 'Batata');
-console.log(comidas);
+const objetosCursos = arrayCursos.map((curso) => {
+  const titulo = curso.querySelector('h1').innerText;
+  const descricao = curso.querySelector('p').innerText;
+  const aulas = curso.querySelector('.aulas').innerText;
+  const horas = curso.querySelector('.horas').innerText;
+  return {
+    titulo,
+    descricao,
+    aulas,
+    horas,
+  };
+});
+console.log(objetosCursos);
+console.log(arrayCursos);
 
-const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// Arrume os estudantes em ordem alfabética
-let ordemAlf = estudantes.sort();
-console.log(ordemAlf);
-// Inverta a ordem dos estudantes
-let ordemAlte = estudantes.reverse();
-console.log(ordemAlf);
-// Verifique se Joana faz parte dos estudantes
-let exist = estudantes.toString().toLowerCase().includes('joana');
-console.log(exist);
-// Verifique se Juliana faz parte dos estudantes
-exist = estudantes.toString().toLowerCase().includes('juliana');
-console.log(exist);
+// Retorne uma lista com os
+// números maiores que 100
+let guardaValores;
+const numeros = [3, 44, 333, 23, 122, 322, 33];
+let maioresQCem = numeros.filter((n) => n > 100);
+console.log(maioresQCem);
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado'];
+let possui = instrumentos.some((item) => {
+  return item === 'Baixo';
+});
+console.log(possui);
 
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`;
-// Substitua section por ul e div com li,
-// utilizando split e join
-let newHtml = html.split('section').join('ul').split('div').join('li');
-console.log(newHtml);
-
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
-const arrayCopy = carros.slice();
-const remove = carros.pop();
-console.log(arrayCopy);
-console.log(carros);
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99',
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99',
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49',
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35',
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60',
+  },
+];
+let totalCompras;
+let limpaPreco;
+totalCompras = compras.reduce((acumulador, item) => {
+  limpaPreco = +item.preco
+    .toString()
+    .replace('R$', '')
+    .replace(',', '.')
+    .trim();
+  return acumulador + limpaPreco;
+}, 0);
+console.log(totalCompras);
