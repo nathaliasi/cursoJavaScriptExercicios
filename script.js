fa6659e343fdb5c6ccc9207a3e40dc4edf843629
Nathália Sardou <nathaliatrab@outlook.com>
2023-05-12 16:24:25 -0300

    exercicios array

--- script.js
+++ script.js
@@ -1,77 +1,46 @@
-// Utilizando o foreach na array abaixo,
-// some os valores de Taxa e os valores de Recebimento
+const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
+// Remova o primeiro valor de comidas e coloque em uma variável
+const removePrimeiro = comidas.shift();
+console.log(removePrimeiro);
+// Remova o último valor de comidas e coloque em uma variável
+const removeUltimo = comidas.pop();
+console.log(removeUltimo);
 
-const transacoes = [
-  {
-    descricao: 'Taxa do Pão',
-    valor: 'R$ 39',
-  },
-  {
-    descricao: 'Taxa do Mercado',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 99',
-  },
-  {
-    descricao: 'Taxa do Banco',
-    valor: 'R$ 129',
-  },
-  {
-    descricao: 'Recebimento de Cliente',
-    valor: 'R$ 49',
-  },
-];
-let taxaTotal = 0;
-let recebimentoTotal = 0;
-transacoes.forEach((item) => {
-  let limpapreco = +item.valor.toLocaleUpperCase().replace('R$', '').trim();
-  if (item.descricao.toLocaleLowerCase().slice(0, 4) === 'taxa') {
-    taxaTotal += limpapreco;
-  } else if (item.descricao.toLocaleLowerCase().slice(0, 4) === 'rece') {
-    recebimentoTotal += limpapreco;
-  }
-});
+// Adicione 'Arroz' ao final da array
+const addFinal = comidas.push('Arroz');
+console.log(comidas);
+// Adicione 'Peixe' e 'Batata' ao início da array
+const addInicio = comidas.unshift('Peixe', 'Batata');
+console.log(comidas);
 
-console.log(taxaTotal);
-console.log(recebimentoTotal);
-// Retorne uma array com a lista abaixo
-const transportes = 'Carro;Avião;Trem;Ônibus;Bicicleta';
-let newArray = transportes.split(', ');
-console.log(newArray);
+const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
+// Arrume os estudantes em ordem alfabética
+let ordemAlf = estudantes.sort();
+console.log(ordemAlf);
+// Inverta a ordem dos estudantes
+let ordemAlte = estudantes.reverse();
+console.log(ordemAlf);
+// Verifique se Joana faz parte dos estudantes
+let exist = estudantes.toString().toLowerCase().includes('joana');
+console.log(exist);
+// Verifique se Juliana faz parte dos estudantes
+exist = estudantes.toString().toLowerCase().includes('juliana');
+console.log(exist);
 
-// Substitua todos os span's por a's
-const html = `<ul>
-                <li><span>Sobre</span></li>
-                <li><span>Produtos</span></li>
-                <li><span>Contato</span></li>
-              </ul>`;
-
-let newHtml = html.split('span').join('a');
-// html.forEach((item) => {
-
-// })
+let html = `<section>
+              <div>Sobre</div>
+              <div>Produtos</div>
+              <div>Contato</div>
+            </section>`;
+// Substitua section por ul e div com li,
+// utilizando split e join
+let newHtml = html.split('section').join('ul').split('div').join('li');
 console.log(newHtml);
-// Retorne o último caracter da frase
-const frase = 'Melhor do ano!';
 
-let newfrase = frase.slice(length - 1);
-
-console.log(newfrase);
-
-// Retorne o total de taxas
-const transacoes2 = [
-  'Taxa do Banco',
-  '   TAXA DO PÃO',
-  '  taxa do mercado',
-  'depósito Bancário',
-  'TARIFA especial',
-];
-let itemMod;
-let soma = 0;
-transacoes2.forEach((item) => {
-  itemMod = item.toUpperCase().trim().slice(0, 4);
-  if (itemMod === 'TAXA') soma++;
-});
-console.log(soma);
+const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
+// Remova o último carro, mas antes de remover
+// salve a array original em outra variável
+const arrayCopy = carros.slice();
+const remove = carros.pop();
+console.log(arrayCopy);
+console.log(carros);
